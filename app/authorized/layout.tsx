fbab37930f0bfbcbcaf2d1c7d188ab5e5641a085
Tobias Bjerregaard <tbjerr22@student.aau.com>
2025-11-04 10:32:43 +0100

    Cleanup

--- app/authorized/layout.tsx
+++ app/authorized/layout.tsx
@@ -1,11 +1,21 @@
-"use client";
+import { Navbar } from "@/components/custom/navbar";
+import SessionProviderClient from "@/components/custom/session-provider-client";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 
-import { SessionProvider } from "next-auth/react";
+export default async function Layout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const session = await getServerSession(authOptions);
 
-export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex h-screen flex-col">
-      <SessionProvider>{children}</SessionProvider>
+      <SessionProviderClient session={session}>
+        <Navbar />
+        <main className="flex-1 min-h-0">{children}</main>
+      </SessionProviderClient>
     </div>
   );
 }
