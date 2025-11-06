31fae0a6146df402039428375387deb51df811a1
Bech <dbech22@student.aau.dk>
2025-11-06 15:10:17 +0100

    Its here

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -64,7 +64,6 @@ export const authOptions = {
             await createUserWithOAuth({
               email: user.email,
               name: user.name,
-              image: user.image,
               password: null,
               userid: userid,
               provider: account.provider,
