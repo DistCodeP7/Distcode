190b1c74994637efc9a10705252445eb896f2be6
MertzAndreas <Andreasmertz11@gmail.com>
2025-11-29 13:03:07 +0100

    Done with the looks

--- components/ui/card.tsx
+++ components/ui/card.tsx
@@ -1,13 +1,14 @@
 import * as React from "react";
 
 import { cn } from "@/lib/utils";
+import { Separator } from "./separator";
 
 function Card({ className, ...props }: React.ComponentProps<"div">) {
   return (
     <div
       data-slot="card"
       className={cn(
-        "bg-card text-card-foreground   flex flex-col gap-6 rounded-xl border shadow-sm",
+        "bg-card text-card-foreground   flex flex-col gap-4 rounded-xl border shadow-sm",
         className,
       )}
       {...props}
@@ -21,12 +22,12 @@ function CardHeader({ className, ...props }: React.ComponentProps<"div">) {
     <div
       data-slot="card-header"
       className={cn(
-        "@container/card-header grid auto-rows-min grid-rows-[auto_auto] items-start gap-1.5 px-6 has-data-[slot=card-action]:grid-cols-[1fr_auto]",
+        "@container/card-header grid auto-rows-min grid-rows-[auto_auto] mt-4 items-start gap-1.5 px-6 has-data-[slot=card-action]:grid-cols-[1fr_auto]",
         className,
       )}
       {...props}
     />
-<div className="h-px bg-border" />
+    <Separator />
 
 
     </>
