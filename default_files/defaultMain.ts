09a3d95e11ff48b25ddcf1577ce0cd2790c01faf
AndersMariegaard <Amarie20@student.aau.dk>
2025-12-10 15:52:26 +0100

    Updated default files

--- default_files/defaultMain.ts
+++ default_files/defaultMain.ts
@@ -1,10 +1,33 @@
-export const defaultMain = `package main
+export const defaultMain = `
+package main
 
 import (
-  "os"
-  "strings"
+    "os"
+    "strings"
+
+    //"runner/shared"
+
+    "github.com/distcodep7/dsnet/dsnet"
 )
+
+var (
+    Peers []string
+    Id    string
+)
+
+func(n *dsnet.Node) Run() {
+    n.Send(dsnet.BaseMessage{
+        From: Id,
+        To:   "TESTER",
+        payload: "Hello, World!",
+    })
+    return
+}
+
 func main(){
     Peers = strings.Split(os.Getenv("PEERS"), ",")
     Id = os.Getenv("ID")
+
+    Node := dsnet.NewNode(Id)
+    Node.Run()
 }`;
--- default_files/defaultProtocol.ts
+++ default_files/defaultProtocol.ts
@@ -1,2 +1,12 @@
 export const defaultProtocol = `package shared
+
+
+// BaseMessage from DSNet, base your protocol on this struct.
+// type BaseMessage struct {
+// 		From string
+// 		To   string
+// 		Type string
+// }
+
+
 `;
