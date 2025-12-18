72c6a9450788db7347ee695687bbe3ea919067e6
bacon <dbech22@student.aau.dk>
2025-12-18 16:13:41 +0100

    one migration

--- drizzle/migrations.ts/0023_regular_abomination.sql
+++ drizzle/migrations.ts/0023_regular_abomination.sql
@@ -2,5 +2,4 @@ ALTER TABLE "job_results" ADD COLUMN "time_reserving" bigint;--> statement-break
 ALTER TABLE "job_results" ADD COLUMN "time_compiling" bigint;--> statement-breakpoint
 ALTER TABLE "job_results" ADD COLUMN "time_running" bigint;--> statement-breakpoint
 ALTER TABLE "job_results" ADD COLUMN "time_pending" bigint;--> statement-breakpoint
-ALTER TABLE "job_results" ADD COLUMN "time_configuring_network" bigint;--> statement-breakpoint
-ALTER TABLE "job_results" ADD COLUMN "time_completed" bigint;
+ALTER TABLE "job_results" ADD COLUMN "time_configuring_network" bigint;
