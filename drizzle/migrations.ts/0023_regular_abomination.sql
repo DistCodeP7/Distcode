3ea3de111e8bf043611e3c2e5edf0c2ce0e0a907
Daniel R Bech <dbech22@student.aau.dk>
2025-12-18 16:19:28 +0100

    Db stuff (#140)
    * db additions

---
+++ drizzle/migrations.ts/0023_regular_abomination.sql
@@ -0,0 +1,5 @@
+ALTER TABLE "job_results" ADD COLUMN "time_reserving" bigint;--> statement-breakpoint
+ALTER TABLE "job_results" ADD COLUMN "time_compiling" bigint;--> statement-breakpoint
+ALTER TABLE "job_results" ADD COLUMN "time_running" bigint;--> statement-breakpoint
+ALTER TABLE "job_results" ADD COLUMN "time_pending" bigint;--> statement-breakpoint
+ALTER TABLE "job_results" ADD COLUMN "time_configuring_network" bigint;
